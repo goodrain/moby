3c5f96e27c5399547bed79dddf90609ed3669fd8
barnett <zengqg@goodrain.com>
2017-05-25 14:02:50 +0800

    测试小程序

---
+++ daemon/logger/zmqlog/test/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/docker/docker/daemon/logger"
+	"github.com/docker/docker/daemon/logger/zmqlog"
+	"github.com/pborman/uuid"
+)
+
+func main() {
+	http.HandleFunc("/start", start)
+	http.HandleFunc("/stop", stop)
+	logrus.Info("start listen")
+	err := http.ListenAndServe("0.0.0.0:10001", nil)
+	if err != nil {
+		logrus.Error("websocket listen error.", err.Error())
+	}
+}
+
+var cache = make(map[string]chan struct{})
+var cacheLog = make(map[string]logger.Logger)
+
+func start(w http.ResponseWriter, r *http.Request) {
+	log, err := zmqlog.New(logger.Context{
+		ContainerID:  uuid.New(),
+		ContainerEnv: []string{"TENANT_ID=" + uuid.New(), "SERVICE_ID=" + uuid.New()},
+		Config:       map[string]string{"zmq-address": "tcp://127.0.0.1:6362"},
+	})
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	stop := make(chan struct{})
+	go send(log, stop)
+	cache[r.FormValue("key")] = stop
+	cacheLog[r.FormValue("key")] = log
+	logrus.Info("Start a log")
+}
+func stop(w http.ResponseWriter, r *http.Request) {
+	if ch, ok := cache[r.FormValue("key")]; ok {
+		close(ch)
+	}
+	if log, ok := cacheLog[r.FormValue("key")]; ok {
+		log.Close()
+	}
+	logrus.Info("close a log")
+}
+
+func send(log logger.Logger, stop chan struct{}) {
+	for {
+		err := log.Log(&logger.Message{
+			Line:      []byte("hello word"),
+			Timestamp: time.Now(),
+			Source:    "stdout",
+		})
+		if err != nil {
+			logrus.Fatal(err)
+		}
+		select {
+		case <-stop:
+			return
+		case <-time.Tick(time.Second):
+		}
+	}
+}
